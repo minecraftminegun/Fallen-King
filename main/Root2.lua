bb663415d43257351e8fc581126972082fa439bf
minecraftminegun <159155336+minecraftminegun@users.noreply.github.com>
2024-07-01 16:39:47 +0200

    Create Root2.lua

---
+++ main/Root2.lua
@@ -0,0 +1,13 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/minecraftminegun/Fallen-King-Hub/main/main%20/Fallen%20King%20Hub%20V1.30%20%5BBETA%5D%20/Root2.lua'))()
+
+if game.PlaceId == 3260590327 then -- ⚡ACE REWORK⚡ Tower Defense Simulator
+game.Players.LocalPlayer:Kick("You're not allowed to exploit at this game.")
+elseif game.PlaceId == 15586387048 then -- 🐾[1100+] Furry&Catboy-Catgirl Clothing&Outfits
+game.Players.LocalPlayer:Kick("You're a FURRY, hands up skid.")
+elseif game.PlaceId == 4739557376 then -- Tower Blitz
+game.Players.LocalPlayer:Kick("You're not allowed to exploit at this game.")
+elseif game.PlaceId == 17321655937 then -- [Upd] Women tycoon
+game.Players.LocalPlayer:Kick("You're such a simp and not allowed to exploit on girl.")
+elseif game.PlaceId == 4483381587 then -- a literal baseplate.
+game.Players.LocalPlayer:Kick("You don't have to be testing this script here, it is already tested and works well.")
+end
